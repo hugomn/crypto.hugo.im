537d679055679c48aa03b306e9d18ec4fd4cc8f1
Hugo Nogueira <hugomn@gmail.com>
2022-01-11 14:28:06 -0300

    feat: add RUNE

--- src/config/coins.ts
+++ src/config/coins.ts
@@ -29,6 +29,7 @@ export const coingeckoCoins: Coin[] = [
   { id: "chainlink", symbol: "LINK" },
   { id: "terra-luna", symbol: "LUNA" },
   { id: "render-token", symbol: "RNDR" },
+  { id: "rune", symbol: "rune" },
   { id: "shiba-inu", symbol: "SHIB" },
   { id: "solana", symbol: "SOL" },
   { id: "vechain", symbol: "VET" },
